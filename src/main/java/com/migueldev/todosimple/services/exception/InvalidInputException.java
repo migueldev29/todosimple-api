b4a7c00b590f8b41e41faec75d65c28687884b76
Miguel Francischini <migueldev29@gmail.com>
2024-05-28 21:46:16 -0300

    Create InvalidInputException

---
+++ src/main/java/com/migueldev/todosimple/services/exception/InvalidInputException.java
@@ -0,0 +1,10 @@
+package com.migueldev.todosimple.services.exception;
+
+public class InvalidInputException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public InvalidInputException(String msg){
+        super(msg);
+    }
+}
