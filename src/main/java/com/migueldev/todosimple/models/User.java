87b81f94a59a5c797dcbe23c98a2e35c77e80c90
Miguel Francischini <migueldev29@gmail.com>
2024-06-03 12:20:35 -0300

    Add lombok and commentary in script

--- src/main/java/com/migueldev/todosimple/models/User.java
+++ src/main/java/com/migueldev/todosimple/models/User.java
@@ -14,14 +14,23 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 
 @Entity
 @Table(name = User.TABLE_NAME)
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
 public class User {
     public interface CreateUser {}
     public interface UpdateUser {}
@@ -47,102 +56,6 @@ public class User {
     private String password;
 
     @OneToMany(mappedBy = "user") //Relacionamento(um usuário pode ter várias tasks) mapeado lá dentro de tasks pelo atributo user
+    @JsonProperty(access = Access.WRITE_ONLY)
     private List<Task> tasks = new ArrayList<Task>();
-
-    public User() {
-    }
-
-    public User(Long id, String username, String password) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-    }
-
-    public Long getId() {
-        return this.id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return this.username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return this.password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @JsonIgnore
-    public List<Task> getTasks() {
-        return this.tasks;
-    }
-
-    public void setTasks(List<Task> tasks) {
-        this.tasks = tasks;
-    }
-
-    public User id(Long id) {
-        setId(id);
-        return this;
-    }
-
-    public User username(String username) {
-        setUsername(username);
-        return this;
-    }
-
-    public User password(String password) {
-        setPassword(password);
-        return this;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this)
-            return true;
-        if (obj == null)
-            return false;
-        if (!(obj instanceof User))
-            return false;
-        User other = (User) obj;
-        if (this.id == null)
-            if (other.id != null)
-                return false;
-            else if (!this.id.equals(other.id))
-                return false;
-        return Objects.equals(this.id, other.id) && Objects.equals(this.username, other.username)
-            && Objects.equals(this.password, other.password);
-    }
-    
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "{" +
-            " id='" + getId() + "'" +
-            ", username='" + getUsername() + "'" +
-            ", password='" + getPassword() + "'" +
-            "}";
-    }
-   
-
-
-    
 }
--- view/script.js
+++ view/script.js
@@ -1,11 +1,13 @@
+//Url de endpoint
 const url = "http://localhost:8080/task/user/1";
 
 function hideLoader() {
+    //Altera a propriedade display para "none" do elemento "loading", tornando invisivel.
     document.getElementById("loading").style.display = "none";
 }
 
 function show(tasks) {
-
+    //Cabeçalho da tabela
     let tab = `<thead>
             <th scope="col">#</th>
             <th scope="col">Description</th>
@@ -13,6 +15,7 @@ function show(tasks) {
             <th scope="col">User Id</th>
         </thead>`;
 
+    //Linhas de dados
     for (let task of tasks) {
         tab += `
             <tr>
@@ -24,17 +27,21 @@ function show(tasks) {
             `;
     }
 
+    //Insere a tabela carregada no corpo do HTML dentro do elemento com id = tasks
     document.getElementById("tasks").innerHTML = tab;
 }
 
 async function getAPI(url){
+    //Executa a requisição na URL com método GET e recebe o retorno
     const response = await fetch(url, { method: "GET"});
     var data = await response.json();
 
     console.log(data);
 
+    //Se tiver um retorno no json então esconder o Loader e mostrar os dados.
     if(response) hideLoader();
     show(data);
 }
 
+//Executa a função getAPI
 getAPI(url);
